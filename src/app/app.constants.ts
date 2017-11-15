bad4c0d8c96d10bb8ec8118ea11907efd353265d
Q-Flash <mubarakdcricketer@hotmail.com>
2017-11-15 00:01:03 -0400

    Initial commit

---
+++ src/app/app.constants.ts
@@ -0,0 +1,21 @@
+/** firebase config */
+export const firebaseConfig = {
+    apiKey: "AIzaSyApGx95wiFEf1CMeD5Z-stQGhfhxf1WRDI",
+    authDomain: "clubsapp-7ac45.firebaseapp.com",
+    databaseURL: "https://clubsapp-7ac45.firebaseio.com",
+    projectId: "clubsapp-7ac45",
+    storageBucket: "",
+    messagingSenderId: "981858796576"
+};
+
+/** google webClientId */
+export const googleWebClientId = 'GOOGLE WEB CLIENT ID';
+
+/** firebase tables */
+export const tableNames = {
+    User               : 'users',
+    Contacts           : 'contacts',
+    ChatChannel        : 'chat_channels',
+    ChatMember         : 'chat_members',
+    ChatMessage        : 'chat_messages',
+};
